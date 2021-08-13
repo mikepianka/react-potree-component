05a1288de39264aa5e2f8471215442cd1f3bcee7
Mike Pianka <47948499+mikepianka@users.noreply.github.com>
2021-08-13 12:06:45 -0400

    move config from viewer.html to component

--- src/components/PotreePointcloud.js
+++ src/components/PotreePointcloud.js
@@ -1,16 +1,81 @@
 import { useEffect, useRef, useState } from "react";
 
 export default function PotreePointcloud() {
-  const potreeIframe = useRef(null);
+  const iframe = useRef(null);
+  const potreeLib = useRef(null);
+  const potreeViewer = useRef(null);
+  const [loaded, setLoaded] = useState(false);
+
+  // initialize a reference to the Potree lib and viewer
+  useEffect(() => {
+    if (iframe.current && loaded) {
+      potreeLib.current = iframe.current.contentDocument.defaultView.Potree;
+      potreeViewer.current = iframe.current.contentDocument.defaultView.viewer;
+    }
+  }, [loaded]);
+
+  // viewer config
+  useEffect(() => {
+    if (loaded && potreeViewer.current && potreeLib.current) {
+      const Potree = potreeLib.current;
+      const viewer = potreeViewer.current;
+
+      viewer.setEDLEnabled(true);
+      viewer.setFOV(60);
+      viewer.setPointBudget(1_000_000);
+      viewer.loadSettingsFromURL();
+      viewer.setBackground("gradient");
+      viewer.setDescription("potree component");
+
+      const controls = new Potree.EarthControls(viewer);
+      viewer.setControls(controls);
+
+      viewer.loadGUI(() => {
+        viewer.setLanguage("en");
+        // $("#menu_tools").next().show();
+        // $("#menu_clipping").next().show();
+        // viewer.toggleSidebar();
+      });
+
+      console.log("Potree viewer config complete");
+    }
+  }, [loaded]);
+
+  // load pointcloud
+  useEffect(() => {
+    if (loaded && potreeViewer.current && potreeLib.current) {
+      const Potree = potreeLib.current;
+      const viewer = potreeViewer.current;
+
+      Potree.loadPointCloud(
+        "./pointclouds/vol_total/cloud.js",
+        "sigeom.sa",
+        (e) => {
+          let scene = viewer.scene;
+          let pointcloud = e.pointcloud;
+
+          let material = pointcloud.material;
+          material.size = 1;
+          material.pointSizeType = Potree.PointSizeType.FIXED;
+          material.shape = Potree.PointShape.CIRCLE;
+
+          scene.addPointCloud(pointcloud);
+
+          viewer.fitToScreen();
+        }
+      );
+    }
+  }, [loaded]);
 
   return (
     <div style={potreePointcloudStyle}>
       <iframe
         title="3D Pointcloud"
-        id="potree"
+        id="potreeIframe"
         src="potree/viewer.html"
-        ref={potreeIframe}
+        ref={iframe}
         style={iframeStyle}
+        onLoad={() => setLoaded(true)}
       />
     </div>
   );
