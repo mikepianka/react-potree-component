7f1c926393f754430d0ac55045aef16c7994418b
Mike Pianka <47948499+mikepianka@users.noreply.github.com>
2021-08-13 11:23:08 -0400

    put potree in iframe
    GUI relies on too many unscoped styles, will take time to rework that

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import "./App.css";
 import PotreePointcloud from "./components/PotreePointcloud";
 
 function App() {
   return (
     <div className="App">
       <h1>a Potree component in React</h1>
-
       <PotreePointcloud />
     </div>
   );
--- src/components/PotreePointcloud.js
+++ src/components/PotreePointcloud.js
@@ -1,65 +1,31 @@
 import { useEffect, useRef, useState } from "react";
-const Potree = window.Potree;
-const THREE = window.THREE;
 
 export default function PotreePointcloud() {
-  const [potreeViewer, setPotreeViewer] = useState(null);
-  const potreeContainer = useRef(null);
-  const potreeRenderArea = useRef(null);
-
-  useEffect(() => {
-    if (potreeContainer.current && potreeRenderArea.current) {
-      const viewer = new Potree.Viewer(potreeRenderArea.current);
-      setPotreeViewer(viewer);
-
-      viewer.setEDLEnabled(false);
-      viewer.setFOV(60);
-      viewer.setPointBudget(1_000_000);
-      viewer.loadSettingsFromURL();
-      viewer.setBackground("gradient");
-
-      viewer.loadGUI(() => {
-        viewer.setLanguage("en");
-        // $("#menu_tools").next().show();
-        // $("#menu_scene").next().show();
-        // viewer.toggleSidebar();
-      });
-
-      Potree.loadPointCloud(
-        "potree/pointclouds/vol_total/cloud.js",
-        "Sorvilier",
-        (e) => {
-          const scene = viewer.scene;
-          const pointcloud = e.pointcloud;
-
-          const material = pointcloud.material;
-          material.size = 1;
-          material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
-          material.shape = Potree.PointShape.SQUARE;
-
-          scene.addPointCloud(pointcloud);
-
-          viewer.fitToScreen();
-        }
-      );
-    }
-  }, []);
+  const potreeIframe = useRef(null);
 
   return (
-    <div
-      ref={potreeContainer}
-      className="potree_container"
-      style={{ potreePointcloudStyle }}
-    >
-      <div ref={potreeRenderArea} id="potree_render_area"></div>
-      <div id="potree_sidebar_container"> </div>
+    <div style={potreePointcloudStyle}>
+      <iframe
+        title="3D Pointcloud"
+        id="potree"
+        src="potree/viewer.html"
+        ref={potreeIframe}
+        style={iframeStyle}
+      />
     </div>
   );
 }
 
 const potreePointcloudStyle = {
-  height: "100%",
-  width: "100%",
+  display: "flex",
+  height: "600px",
+  width: "800px",
   margin: 0,
   padding: 0,
+};
+
+const iframeStyle = {
+  width: "100%",
+  height: "100%",
+  border: 0,
 };
